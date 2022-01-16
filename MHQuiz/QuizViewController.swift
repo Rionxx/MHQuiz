3f0e4825f23b2b808579bc43945742bd18bb06a9
Rion <rionfx.42@gmail.com>
2022-01-16 10:30:17 +0900

    Confirm button action print

---
+++ MHQuiz/QuizViewController.swift
@@ -0,0 +1,32 @@
+//
+//  QuizViewController.swift
+//  MHQuiz
+//
+//  Created by FX on 2022/01/16.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnAction(sender: UIButton) {
+        print(sender.tag)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
