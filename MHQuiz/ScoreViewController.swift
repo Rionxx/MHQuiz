bc2a156de403432bd116f95737423a0c18805d07
Rion <rionfx.42@gmail.com>
2022-01-18 00:31:18 +0900

    Could display Score and navigate TopView

--- MHQuiz/ScoreViewController.swift
+++ MHQuiz/ScoreViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class ScoreViewController: UIViewController {
-
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correct = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        scoreLabel.text = "\(correct)問正解!"
+        
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func roTopButtonAction(_ sender: Any) {
+        //close Score View and Quiz View
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
     
 
